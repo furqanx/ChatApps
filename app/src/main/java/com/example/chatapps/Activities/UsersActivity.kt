dbbb8f41704334e81426356fa653b99a0de925d2
Dzulkiram Hilmi <126537185+kiramDz@users.noreply.github.com>
2023-05-22 11:59:14 +0700

    Update UsersActivity.kt

--- app/src/main/java/com/example/chatapps/Activities/UsersActivity.kt
+++ app/src/main/java/com/example/chatapps/Activities/UsersActivity.kt
@@ -1,1 +1,105 @@
+/** UsersActivity **/
 package com.example.chatapps.Activities
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.chatapps.R
+import com.example.chatapps.Adapters.UserAdapter
+import com.example.chatapps.Firebase.FirebaseService
+import com.example.chatapps.Models.User
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+//import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+import de.hdodenhof.circleimageview.CircleImageView
+//import kotlinx.android.synthetic.main.activity_users.*
+
+class UsersActivity : AppCompatActivity() {
+    // Mendefinisikan properti komponen-komponen
+    lateinit var imgBack: ImageView
+    lateinit var imgProfile: CircleImageView
+    lateinit var userRecyclerView: RecyclerView
+    var userList = ArrayList<User>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users)
+
+        // Inisialisasi komponen-komponen
+        imgBack = findViewById(R.id.imgBack)
+        imgProfile = findViewById(R.id.imgProfile)
+        userRecyclerView = findViewById(R.id.userRecyclerView)
+
+        FirebaseService.sharedPref = getSharedPreferences("sharedPref", MODE_PRIVATE)
+        FirebaseMessaging.getInstance().token.addOnSuccessListener { token ->
+            FirebaseService.token = token
+        }
+
+        userRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+
+        imgBack.setOnClickListener {
+            onBackPressed()
+        }
+
+        imgProfile.setOnClickListener {
+            val intent = Intent(
+                this@UsersActivity,
+                ProfileActivity::class.java
+            )
+            startActivity(intent)
+        }
+        getUsersList()
+    }
+
+    fun getUsersList() {
+        val firebase: FirebaseUser = FirebaseAuth.getInstance().currentUser!!
+
+        var userid = firebase.uid
+        FirebaseMessaging.getInstance().subscribeToTopic("/topics/$userid")
+
+
+        val databaseReference: DatabaseReference =
+            FirebaseDatabase.getInstance().getReference("Users")
+
+
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(applicationContext, error.message, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userList.clear()
+                val currentUser = snapshot.getValue(User::class.java)
+                if (currentUser!!.profileImage == ""){
+                    imgProfile.setImageResource(R.drawable.profile_image)
+                }else{
+                    Glide.with(this@UsersActivity).load(currentUser.profileImage).into(imgProfile)
+                }
+
+                for (dataSnapShot: DataSnapshot in snapshot.children) {
+                    val user = dataSnapShot.getValue(User::class.java)
+
+                    if (!user!!.userId.equals(firebase.uid)) {
+
+                        userList.add(user)
+                    }
+                }
+
+                val userAdapter = UserAdapter(this@UsersActivity, userList)
+
+                userRecyclerView.adapter = userAdapter
+            }
+
+        })
+    }
+}
